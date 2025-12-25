f2d01ceb730bf1b331aea36ceafda5e4aba2a0ad
Chisanupong Phetruen <Chisanupong.ph@ku.th>
2025-12-25 15:02:07 +0700

    test10

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -18,6 +18,6 @@ describe('Calculator', () => {
     });
 
     test('divides 10 / 2 to equal 5', () => {
-        expect(divide(10, 2)).toBe(5);
+        expect(divide(10, 2)).toBe(10);
     });
 });
